4678d18f58f5cd9973c68d590624fbda919c981d
Abiola Ibrahim <abiola89@gmail.com>
2018-12-03 10:39:16 +0000

    stop docker before copying binaries

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 echo installing docker...
 sudo apt install -y ./docker-ce_18.06.1_ce_3-0_debian_amd64.deb
 
+echo temprarily stopping docker...
+sudo systemctl stop docker
+
 echo copying files...
 sudo cp containerd* runc* docker* /usr/bin
 
 echo restarting docker...
-sudo systemctl restart docker
+sudo systemctl start docker
 
 echo done
 
